d915305eec16933195f76cdac9a52f9a8c394556
IvanZarate <ivan23.zarate25@gmail.com>
2023-05-15 23:39:00 -0300

    railway

--- src/app.js
+++ src/app.js
@@ -44,7 +44,7 @@ else {
         cors({
             origin: whiteList,
             methods: ["POST", "PUT", "GET", "DELETE", "OPTIONS", "HEAD"],
-            header: ["Authorization", "X-API-KEY", "Origin", "X-Requested-With", "Content-Type", "Accept", "Access-Control-Allow-Request-Method", 'Access-Control-Allow-Headers', 'Origin'],
+            header: ["*","Authorization", "X-API-KEY", "Origin", "X-Requested-With", "Content-Type", "Accept", "Access-Control-Allow-Request-Method", 'Access-Control-Allow-Headers', 'Origin'],
             credentials: true,
         })
     );
