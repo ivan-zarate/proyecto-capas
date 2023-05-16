481024d43029835c442e841b2507b4a190496cba
IvanZarate <ivan23.zarate25@gmail.com>
2023-05-15 22:55:06 -0300

    railway

--- src/app.js
+++ src/app.js
@@ -42,7 +42,7 @@ else {
 
     app.use(
         cors({
-            origin: whiteList,
+            //origin: whiteList,
             methods: ["POST", "PUT", "GET", "DELETE", "OPTIONS", "HEAD"],
             header: ["Authorization", "X-API-KEY", "Origin", "X-Requested-With", "Content-Type", "Accept, Access-Control-Allow-Request-Method"],
             credentials: true,
