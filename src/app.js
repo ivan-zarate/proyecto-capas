b104482fe756fe424857aa8c5d82bdfb7f0689f8
IvanZarate <ivan23.zarate25@gmail.com>
2023-05-17 11:03:48 -0300

    railway

--- src/app.js
+++ src/app.js
@@ -97,16 +97,16 @@ else {
     srv.on('error', error => logger.warn(`Error en el servidor ${error}`))
 
     app.get("/",(req,res)=>{
-        const link = 'https://proyecto-capas-production.up.railway.app/server-backend/src/public/index.html';
+        const link = 'http://127.0.0.1:5500/server-backend/src/public/index.html';
         res.redirect(link);
         //res.sendFile(__dirname + '/public/index.html')
     });
 
-    // app.get("*", async (req, res) => {
-    //     const link = 'http://127.0.0.1:5500/server-backend/src/public/index.html'
-    //     logger.warn("No existe la pagina solicitada")
-    //     return res.status(400).redirect(link);
-    // });
+    app.get("*", async (req, res) => {
+        const link = 'http://127.0.0.1:5500/server-backend/src/public/index.html'
+        logger.warn("No existe la pagina solicitada")
+        return res.status(400).redirect(link);
+    });
     // app.get("/app",(req,res)=>{
     //     let baseUrl="";
     //     if (newArgs.app === "prod") {
