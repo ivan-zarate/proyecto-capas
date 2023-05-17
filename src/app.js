5fab459baebbc6da90632488da0512743498e300
IvanZarate <ivan23.zarate25@gmail.com>
2023-05-16 23:44:59 -0300

    railway

--- src/app.js
+++ src/app.js
@@ -52,10 +52,10 @@ else {
     //Creacion de sesiones en mongoStore
     app.use(session({
         store: MongoStore.create({
-            mongoUrl: config.MONGO_SESSION,
+            mongoUrl: options.MONGO_SESSION,
             ttl: 600
         }),
-        secret: config.CLAVE_SECRETA,
+        secret: options.CLAVE_SECRETA,
         resave: false,
         saveUninitialized: false,
         cookie: {
