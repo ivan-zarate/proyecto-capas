deef8757f1c5889bafaf46ff63a1e3a101b7d7d5
IvanZarate <ivan23.zarate25@gmail.com>
2023-05-17 10:31:25 -0300

    railway

--- src/app.js
+++ src/app.js
@@ -96,11 +96,11 @@ else {
 
     srv.on('error', error => logger.warn(`Error en el servidor ${error}`))
 
-    // app.get("/",(req,res)=>{
-    //     const link = 'http://127.0.0.1:5500/server-backend/src/public/index.html';
-    //     //res.redirect(link);
-    //     res.sendFile(__dirname + '/public/index.html')
-    // });
+    app.get("/",(req,res)=>{
+        const link = 'http://127.0.0.1:5500/server-backend/src/public/index.html';
+        res.redirect(link);
+        //res.sendFile(__dirname + '/public/index.html')
+    });
 
     // app.get("*", async (req, res) => {
     //     const link = 'http://127.0.0.1:5500/server-backend/src/public/index.html'
